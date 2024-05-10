28baa6c46516c6e609cf6ecdc7e37c953b7f0bc0
bivek.joshi <bivek.joshi@dghub.io>
2024-05-10 16:05:47 +0545

    changes

---
+++ src/components/custom/Form/Input/PasswordInput/PasswordInput.stories.tsx
@@ -0,0 +1,21 @@
+import { StoryObj, Meta } from "@storybook/react";
+import { PasswordInput } from "./PasswordInput";
+
+const meta: Meta<typeof PasswordInput> = {
+  title: "Form/Input/PasswordInput",
+
+  args: {},
+  component: PasswordInput,
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "Input here ...",
+  },
+};
